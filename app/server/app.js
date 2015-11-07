0ad8b0e685410a35f35e7940252e5cda0fc8b687
yukio yamamoto <aj.yamamoto@me.com>
2015-11-07 10:47:48 -1000

    made changes to architecture

--- app/server/app.js
+++ app/server/app.js
@@ -2,13 +2,6 @@ var express = require('express');
 var app = express();
 
 //Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/../../public'));
-});
 
-//Get the dummy data
-require('./ddata.js');
 
-var port = 8080;
-app.listen(port);
 console.log('Express server started on port %s', port);
