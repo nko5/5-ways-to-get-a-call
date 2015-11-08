63073c16589fe272c54267f55eadc671a23595a6
mdctenga <mdctenga@gmail.com>
2015-11-07 23:24:34 -1000

    Access Twitter trends on client

--- public/js/trigger.js
+++ public/js/trigger.js
@@ -1,18 +1,6 @@
-$(function (){
-  var datas;
-  var url = 'http://www.youtube.com/embed/';
-  $('button').on('click', function (){
-    // $.get('/api/youtube/search')
-    //   .done(function(data) {
-    //     datas = $.parseJSON(data);
-    //     console.log({data : datas.items[0]}); // do the shaking api
-    //     $('iframe').attr('src', url + datas.items[0].id.videoId + '?autoplay=true');
-    //   });
-    $.get('/api/twitter/trends')
-      .done(function(data) {
-        datas = $.parseJSON(data);
-        console.log({data : datas}); // do the shaking api
-      });
-  });
-  
-});
+angular.module('app')
+  .service('trendsService', ['$http', function ($http) {
+    this.getTwitter = function(){
+      return $http.get('api/twitter/trends');
+    };
+}]);
